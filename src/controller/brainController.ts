06e245c311394b5c5788e25cd95b1c07fe430ceb
gopala <gopal1852000@gmail.com>
2025-06-13 16:15:21 +0530

    update and delete brain feature added

--- src/controller/brainController.ts
+++ src/controller/brainController.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { findUserId } from '../services/userService';
-import  {getBrainsAll, createNewBrain} from '../services/brainService';
+import  {getBrainsAll, createNewBrain, getaBrain, updateBrain_db, deleteBrain_db} from '../services/brainService';
 import { Types } from 'mongoose';
 import { Ibrain } from '../models/brainModel';
 import { Itag} from '../models/tagModel';
@@ -22,7 +22,46 @@ export async function getBrains(req: express.Request, res: express.Response){
 
 
 }
-export function updateBrain(req: express.Request, res: express.Response){
+export async function updateBrain(req: express.Request, res: express.Response){
+    //get the id of the and update
+    //make sure person owns the brain
+    
+    let id: string = req.body.id;
+    if(!Types.ObjectId.isValid(id)){
+        res.status(400).json({ msg: "Invalid brain ID format" });
+        return;
+    }
+    const brainID: Types.ObjectId = new Types.ObjectId(req.body.id);
+    
+    
+    const userEmail = req.body.user.email;
+    const userID:Types.ObjectId | null = await findUserId(userEmail);
+    if(!userID){
+        res.status(404).json({
+            msg:"User does not exist"
+        });
+        return;
+    }
+
+    const brainPresent = await getaBrain(brainID, userID);
+    if(!brainPresent){
+        res.status(404).json({
+            msg: `brain with id ${brainID}`
+        })
+        return;
+    }
+
+    const brain:Partial<Ibrain> = {
+        title: req.body.title,
+        text: req.body.text,
+        sharable: req.body.sharable,
+        dateCreated: req.body.dateCreated,
+    };
+    const updatedBrain = await updateBrain_db(brainID, brain);
+
+    res.status(200).json(updatedBrain)
+    return;
+
 
 }
 export async function createBrain(req: express.Request, res: express.Response){
@@ -71,7 +110,7 @@ export async function createBrain(req: express.Request, res: express.Response){
         dateCreated: req.body.dateCreated,
         author: userID
     };
-    console.log(brain);
+    
 
     const createdBrain = await createNewBrain(brain);
     res.status(200).json({
@@ -83,6 +122,34 @@ export async function createBrain(req: express.Request, res: express.Response){
 
     
 }
-export function deleteBrain(req: express.Request, res: express.Response){
+export async function deleteBrain(req: express.Request, res: express.Response){
+    let id: string = req.body.id;
+    if(!Types.ObjectId.isValid(id)){
+        res.status(400).json({ msg: "Invalid brain ID format" });
+        return;
+    }
+    const brainID: Types.ObjectId = new Types.ObjectId(req.body.id);
+    
+    
+    const userEmail = req.body.user.email;
+    const userID:Types.ObjectId | null = await findUserId(userEmail);
+    if(!userID){
+        res.status(404).json({
+            msg:"User does not exist"
+        });
+        return;
+    }
+
+    const brainPresent = await getaBrain(brainID, userID);
+    if(!brainPresent){
+        res.status(404).json({
+            msg: `brain with id ${brainID}`
+        })
+        return;
+    }
+    const deleted_brain = await deleteBrain_db(brainID)
+    res.status(200).json(deleted_brain);
+    return;
+
 
 }
--- src/services/brainService.ts
+++ src/services/brainService.ts
@@ -19,3 +19,32 @@ export async function createNewBrain(brain: Ibrain): Promise<Ibrain> {
         throw new Error(e.message || "Error creating a new brain")
     }
 }
+
+export async function getaBrain(brainID:mongoose.Types.ObjectId , userID:mongoose.Types.ObjectId): Promise<Ibrain | null>{
+    try{
+        console.log("hi there")
+        const brain = await BrainModel.findOne({_id: brainID, author: userID});
+        console.log("hi there")
+        return brain
+    }catch(e: any){
+        throw new Error(e.message || "Error getting a brain")
+    }
+}
+
+export async function updateBrain_db(brainID:mongoose.Types.ObjectId, updateBrain: Partial<Ibrain>): Promise<Ibrain | null> {
+    try{
+        const updatedBrain = await BrainModel.findByIdAndUpdate(brainID, updateBrain);
+        return updatedBrain
+    }catch(e: any){
+        throw new Error(e.message || "Error updating the brain")
+    }
+}
+
+export async function deleteBrain_db(brainID: mongoose.Types.ObjectId): Promise<Ibrain | null>{
+    try{
+        const deletedBrain = BrainModel.findByIdAndDelete({_id: brainID});
+        return deletedBrain;
+    }catch(e: any){
+        throw new Error(e.message || "Error deleting the brain")
+    }
+}
