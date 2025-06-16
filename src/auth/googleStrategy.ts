b02ae0b532daaaec165d077aa9ae1c927b6535a6
gopala <gopal1852000@gmail.com>
2025-06-16 21:52:10 +0530

    yesterdays changes

--- src/auth/googleStrategy.ts
+++ src/auth/googleStrategy.ts
@@ -1,6 +1,7 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy} from "passport-google-oauth20";
 import dotenv from 'dotenv';
+import express from 'express';
 dotenv.config();
 
 const googleStrategy = new GoogleStrategy({
@@ -9,7 +10,12 @@ const googleStrategy = new GoogleStrategy({
     callbackURL: "/auth/google/callback",
     passReqToCallback: true
     //@ts-ignore
-}, async (req, accessToken, refreshToken, profile, done)=>{
+}, async (req: express.Request, accessToken: string, refreshToken: string, profile, done)=>{
+    //get email from google and see if exists in db
+    // y -> user session exist and email does not match => req.user= jack@gmail.com, google -> john@gmail.com
+    // if all good done(null, user)
+    // useremail and google email match but google account not linked
+    // n -> createuser
     const user = {
         googleId: profile.id,
         email: profile.emails?.[0].value,
